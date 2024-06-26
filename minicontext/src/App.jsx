5b88b644796006840b5bf33ff9deba2e92b7e57a
neelesh yadav <neelesh6kumar@gmail.com>
2024-06-26 19:28:17 +0530

    secondcommit

---
+++ minicontext/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import Login from './Component/Login'
+import Profile from './Component/Profile'
+
+import UserContextProvider from './Context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>Context Api </h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
