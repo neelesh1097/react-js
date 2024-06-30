da63b140daff7f3bb94274bb0e8623f763ce2b93
neelesh1097 <neelsh6kumar@gmail.com>
2024-06-30 15:09:39 +0530

    commit

---
+++ mealapiproject/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Index from './Index.jsx'
+function App() {
+ 
+
+  return (
+    <>
+      
+       <Index/>
+      
+    </>
+  )
+}
+
+export default App
